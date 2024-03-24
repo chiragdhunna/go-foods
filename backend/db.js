fdd82509e5d9ad479481e35e1e5145da4c9f64b6
chiragdhunna <chiragdhunna2468@gmail.com>
2024-03-25 02:09:19 +0530

    Uploading Final Project

--- backend/db.js
+++ backend/db.js
@@ -1,29 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const mongoURI = 'mongodb+srv://id:password@cluster0.himdvv4.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI =
+  "mongodb+srv://chigsdroid:password@cluster0.jsnoftw.mongodb.net/";
 
 module.exports = async function (callback) {
-    try {
-        const connectionOptions = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        };
+  try {
+    const connectionOptions = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
 
-        await mongoose.connect(mongoURI, connectionOptions);
+    await mongoose.connect(mongoURI, connectionOptions);
 
-        console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
 
-        const foodCollection = mongoose.connection.db.collection('food_items');
-        const categoryCollection = mongoose.connection.db.collection('Categories');
+    const foodCollection = mongoose.connection
+      .useDb("gofoodmern")
+      .collection("food_items");
+    const categoryCollection = mongoose.connection
+      .useDb("gofoodmern")
+      .collection("foodCategory");
 
-        const [foodData, categoryData] = await Promise.all([
-            foodCollection.find({}).toArray(),
-            categoryCollection.find({}).toArray()
-        ]);
+    const [foodData, categoryData] = await Promise.all([
+      foodCollection.find({}).toArray(),
+      categoryCollection.find({}).toArray(),
+    ]);
 
-        callback(null, foodData, categoryData);
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        callback(error, null, null);
-    }
+    console.log(foodData);
+    console.log(foodCollection);
+
+    callback(null, foodData, categoryData);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    callback(error, null, null);
+  }
 };
